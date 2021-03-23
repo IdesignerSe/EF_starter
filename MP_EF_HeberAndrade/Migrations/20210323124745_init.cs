57a5ee6912abeae18d31b69eb65574e31ab64f62
idesignerse <heberandrade@gmail.com>
2021-03-23 13:55:57 +0100

    Installation Starter Packages Nugets
    Add Entity FrameWork
    From Package Manage Console
    
    https://docs.microsoft.com/en-us/ef/core/get-started/overview/install
    
    Install
    1 Entity framework
    dotnet add package Microsoft.EntityFrameworkCore
    
    2 Sql Server
    dotnet add package Microsoft.EntityFrameworkCore.SqlServer
    
    3 Tools
    
    dotnet tool install --global dotnet-ef
    and
    Install-Package Microsoft.EntityFrameworkCore.Tools
    
    4 update tolls
    dotnet tool update
    
    5 Instal the latest Design
    dotnet add package Microsoft.EntityFrameworkCore.Design
    
    kolla på Nugets om man ses alla under Nuget
    
    then you most do 1 Add-Migration init
    2 Update-Database
    It looks like these as result
    ---------------------------
    PM> Add-Migration init
    Build started...
    Build succeeded.
    To undo this action, use Remove-Migration.
    PM> Update-Database
    Build started...
    Build succeeded.
    Applying migration '20210323104849_init'.
    Done.
    PM>
    -------------------

---
+++ MP_EF_HeberAndrade/Migrations/20210323124745_init.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MP_EF_HeberAndrade.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DietName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DietType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DietSelect = table.Column<int>(type: "int", nullable: false),
+                    DietDaysProgram = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Diets1",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DietName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DietType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DietSelect = table.Column<int>(type: "int", nullable: false),
+                    DietDaysProgram = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diets1", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Diets");
+
+            migrationBuilder.DropTable(
+                name: "Diets1");
+        }
+    }
+}
--- MP_EF_HeberAndrade/Program.cs
+++ MP_EF_HeberAndrade/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace MP_EF_HeberAndrade
 {
@@ -9,4 +13,57 @@ namespace MP_EF_HeberAndrade
             Console.WriteLine("Hello World!");
         }
     }
+
+
+
+    public class Diet : CustomTreatment
+    {
+        public Diet(string dietName, string dietType, int dietSelect, int dietDaysProgram)
+        {
+            DietName = dietName;
+            DietType = dietType;
+            DietSelect = dietSelect;
+            DietDaysProgram = dietDaysProgram;
+        }
+        public int Id { get; set; }
+    }
+
+
+
+    public class Diet1 : CustomTreatment
+    {
+        public Diet1(string dietName, string dietType, int dietSelect, int dietDaysProgram)
+        {
+            DietName = dietName;
+            DietType = dietType;
+            DietSelect = dietSelect;
+            DietDaysProgram = dietDaysProgram;
+        }
+        public int Id { get; set; }
+    }
+
+
+
+    public class CustomTreatment
+    {
+        public string DietName { get; set; }
+        public string DietType { get; set; }
+        public int DietSelect { get; set; }
+        public int DietDaysProgram { get; set; }
+    }
+
+
+
+    class CustomTreatmentContext : DbContext
+    {
+        public DbSet<Diet> Diets { get; set; }
+        public DbSet<Diet1> Diets1 { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //s5d011\sqlexpres
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = CustomTreatment; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
 }
+
+
